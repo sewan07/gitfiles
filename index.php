c657fb7cc16c583e7d70ed868e2f196bbc6a96da
sewan <kido184@hotmail.com>
2016-11-25 15:16:57 +0000

    intial commit

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
 </head>
 <body>
 <p>
-    <?php
-    echo "Hello, world!";
-    echo "Hello," . " " . "world" . "!";
-    ?>
+<?php
+// connect to csdm- webdev server and sel ect database
+$db = new mysqli(
+"hostname",
+"username",
+"password",
+"dbname"
+);
+// test if connection was established, and print any errors
+if($db->connect_errno){
+die("Connectfailed['.$db->connect_error.']");
+}
+?>
+
 </p>
 </body>
 </html>
